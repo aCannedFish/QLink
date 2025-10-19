051a535619470b71847ffbda92cd74c9d8b57c5c
Yuguanting <yuguanting@sjtu.edu.cn>
2025-10-19 18:45:31 +0800

    Nope

--- QLink/main.cpp
+++ QLink/main.cpp
@@ -1,10 +1,10 @@
-// #include "menu.h"
-// #include <QtWidgets/QApplication>
+#include "menu.h"
+#include <QtWidgets/QApplication>
 
-// int main(int argc, char *argv[])
-// {
-//     QApplication app(argc, argv);
-//     Menu window;
-//     window.show();
-//     return app.exec();
-// }
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Menu window;
+    window.show();
+    return app.exec();
+}
